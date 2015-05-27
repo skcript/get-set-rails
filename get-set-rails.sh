e3692db0fb2b5fc78e00e8be7cac60bffb05a5f9
Karthik K <karthik@skcript.com>
2015-05-27 12:48:52 +0530

    🚀Preparing for init release

---
+++ get-set-rails.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Get. Set. Rails.
+#
+# Licence: MIT
+# Maintained by: DevOps Team at Skcript
+# 
+# Please reach out to us if you have any queries. bello@skcript.com
+
+echo "#####################################"
+echo "########## Get. Set. Rails ##########"
+echo "#####################################"
